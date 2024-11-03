6e231178f80e86206339b4cffd73b5592a5792d6
SalehRahmanzade <salehrahmanzade@gmail.com>
2024-11-03 21:23:31 +0330

    tamrin 13 a

---
+++ t.py
@@ -0,0 +1,21 @@
+a,b = 1 , "saleh";
+d,c = 3.4 , 2;
+print(a,b,d,c);
+print(type(a),type(b),type(d),type(c));
+
+x = int(input("Enter a number:"));
+y = float(input("Enter a float:"));
+z = str(input("Enter a name:"));
+
+print(x,y,z);
+print(type(x),type(y),type(z));
+
+y = int(y);
+
+print( "x + y => " , x + y );
+print( "x - y => " , x - y );
+print( "x * y => " , x * y );
+print( "x / y => " , x / y );
+print( "x % y => " , x % y );
+print( "x ** y => " , x ** y );
+print( "x // y => " , x // y );
